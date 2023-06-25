3dc9b4ab77b8353d335439119b32a78038819128
Thiago de Loreto <thiagodeloretotreichel@gmail.com>
2023-06-24 23:12:33 -0300

    Add files via upload

---
+++ comunidadelemur/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = '25c4274e4cf317c1e0dc2c0cf21c1b12'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Faça login para acessar a página'
+login_manager.login_message_category = 'alert-info'
+
+from comunidadelemur import routes
