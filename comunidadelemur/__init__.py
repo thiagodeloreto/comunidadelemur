3621098a82cab6e1c34ff44f7b5f5a720fc53fff
Thiago de Loreto <thiagodeloretotreichel@gmail.com>
2023-06-24 23:42:29 -0300

    Update __init__.py

--- comunidadelemur/__init__.py
+++ comunidadelemur/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 
@@ -20,4 +21,17 @@ login_manager.login_view = 'login'
 login_manager.login_message = 'Faça login para acessar a página'
 login_manager.login_message_category = 'alert-info'
 
+from comunidadelemur import models
+
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table('usuario'):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print('Base de Dados criado')
+else:
+    print('Base de Dados já existente')
+
 from comunidadelemur import routes
+
