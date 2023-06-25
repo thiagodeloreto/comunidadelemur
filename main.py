2d7ada19614fcb00e1a5e90528ee50ff4ea65af9
Thiago de Loreto <thiagodeloretotreichel@gmail.com>
2023-06-25 01:18:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from comunidadelemur import app
+from gevent.pywsgi import WSGIServer
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
